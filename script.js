b57b333e228f1211e083dcfde1f786a9c3bcb0aa
bltakk <gevorg.hovh.2005@gmail.com>
2026-03-06 13:13:54 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,84 @@
-window.onload = function(){
-let music = document.getElementById("music");
-music.volume = 0.2;   // ցածր ձայն
+const pages = [
+"Թող գարունները գան ու չգնան… և որովհետև կինն է աշխարհի ամենաերկարատև գարունը։ Ու թող ձեր ժպիտներն ու դրական էներգիան միշտ լինեն բոլորի հարատև ուղեկիցը",
+"Երկրորդ էջի տեքստ",
+"Երրորդ էջի տեքստ",
+"Չորրորդ էջի տեքստ",
+"Հինգերորդ էջի տեքստ"
+];
+
+let current = 0;
+
+const textEl = document.getElementById("text");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+
+function updatePage(){
+textEl.textContent = pages[current];
+
+prevBtn.style.display = (current === 0) ? "none" : "inline";
+nextBtn.style.display = (current === pages.length-1) ? "none" : "inline";
 }
+
+updatePage();
+
+prevBtn.addEventListener("click",()=>{
+if(current>0){
+current--;
+updatePage();
+}
+});
+
+nextBtn.addEventListener("click",()=>{
+if(current<pages.length-1){
+current++;
+updatePage();
+}
+});
+
+let music;
+const btn=document.getElementById("playBtn");
+
+btn.addEventListener("click",()=>{
+
+if(!music){
+music=new Audio("music.mp3");
+music.loop=true;
+music.volume=0.2;
+music.play();
+btn.textContent="Կանգնեցնել 🎵";
+}
+
+else if(music.paused){
+music.play();
+btn.textContent="Կանգնեցնել 🎵";
+}
+
+else{
+music.pause();
+btn.textContent="Միացնել 🎵";
+}
+
+});
+
+const flowers=["🌸","🌷","🌼","🌺","💐"];
+
+function createPetal(){
+
+const petal=document.createElement("div");
+petal.classList.add("petal");
+
+petal.innerHTML=flowers[Math.floor(Math.random()*flowers.length)];
+
+petal.style.left=Math.random()*100+"vw";
+petal.style.animationDuration=(5+Math.random()*5)+"s";
+petal.style.fontSize=(16+Math.random()*12)+"px";
+
+document.body.appendChild(petal);
+
+setTimeout(()=>{
+petal.remove();
+},11000);
+
+}
+
+setInterval(createPetal,600);
